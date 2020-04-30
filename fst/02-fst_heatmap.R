bb3c6ee0b340a6556bcee4ad6226c66331048a65
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2020-04-30 15:20:36 -0500

    Add files via upload

---
+++ fst/02-fst_heatmap.R
@@ -0,0 +1,23 @@
+#!/usr/bin/Rscript
+setwd("E:/Evelien's Dropbox/Dropbox/PUMA/Bioinformatics/reseq/FST")
+
+fst <- read.csv("fst_67_quickview.csv")
+fst_table <- fst[,c(2,3,4,5)]
+
+library(pheatmap)
+fst_matrix <- data.matrix(fst_table)
+heatmap_test <- heatmap(fst_matrix)
+
+colorset <- c("beige", "purple")
+heatmap <- pheatmap(fst_matrix, 
+                    color=colorRampPalette(colorset)(50),
+                    cluster_rows=F,
+                    cluster_cols=F,
+                    border_color="gray", 
+                    cellwidth=80, 
+                    cellheight=80,
+                    legend=TRUE,
+                    main="FSTs with 4 pops",
+                    na_col="white",
+                    filename="fst_heatmap.jpeg"  #this automatically saves in directory
+)
