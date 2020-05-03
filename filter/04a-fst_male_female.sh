eddcb0422fc91335230b938d313c0b9b5b833ab4
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2020-05-03 00:27:50 -0500

    Update 04a-fst_male_female.sh

--- filter/04a-fst_male_female.sh
+++ filter/04a-fst_male_female.sh
@@ -2,7 +2,7 @@
 
 #Running FST between samples of males and females to identify scaffolds of high differences
 
-input_vcf=imputed.qualityfiltered.recode.vcf
+input_vcf=imputed.qualityfiltered.LD.recode.vcf
 males=list_males
 females=list_females
 module load VCFtools/0.1.16-intel-2018b-Perl-5.28.0
